1d016d4fadea9c17c952fa7ec9c63d261dcf5afd
34j <34j.github@proton.me>
2025-08-22 13:19:20 +0900

    feat: wip

--- src/jacobi_poly/__init__.py
+++ src/jacobi_poly/__init__.py
@@ -1,1 +1,21 @@
 __version__ = "0.0.0"
+from ._lgamma import binom, lgamma
+from ._main import (
+    gegenbauer,
+    jacobi,
+    jacobi_normalization_constant,
+    legendre,
+    log_jacobi_normalization_constant,
+)
+from ._triplet import jacobi_triplet_integral
+
+__all__ = [
+    "binom",
+    "gegenbauer",
+    "jacobi",
+    "jacobi_normalization_constant",
+    "jacobi_triplet_integral",
+    "legendre",
+    "lgamma",
+    "log_jacobi_normalization_constant",
+]
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,100 @@
-from jacobi_poly.main import add
+from typing import Literal
+
+import array_api_extra as xpx
+import pytest
+from array_api._2024_12 import ArrayNamespaceFull
+from scipy.special import eval_gegenbauer, eval_jacobi
+
+from jacobi_poly import binom, gegenbauer, jacobi, legendre
 
 
-def test_add():
-    """Adding two number works as expected."""
-    assert add(1, 1) == 2
+@pytest.mark.parametrize(
+    "shape",
+    [
+        (1,),
+        (2, 3),
+        (3, 3, 4),
+    ],
+)
+@pytest.mark.parametrize("n_end", [8])
+def test_jacobi(shape: tuple[int, ...], n_end: int, xp: ArrayNamespaceFull) -> None:
+    alpha = xp.random.random_uniform(low=0, high=5, shape=shape)
+    beta = xp.random.random_uniform(low=0, high=5, shape=shape)
+    x = xp.random.random_uniform(low=0, high=1, shape=shape)
+    n = xp.arange(n_end)[
+        (None,) * len(shape)
+        + (
+            slice(
+                None,
+            ),
+        )
+    ]
+    expected = eval_jacobi(n, alpha[..., None], beta[..., None], x[..., None])
+    actual = jacobi(x, alpha=alpha, beta=beta, n_end=n_end)
+    assert xp.all(xpx.isclose(expected, actual, rtol=1e-3, atol=1e-3))
+
+
+@pytest.mark.parametrize(
+    "shape",
+    [
+        (1,),
+        (2, 3),
+        (3, 3, 4),
+    ],
+)
+@pytest.mark.parametrize("n_end", [8])
+def test_gegenbauer(shape: tuple[int, ...], n_end: int, xp: ArrayNamespaceFull) -> None:
+    alpha = xp.random.random_uniform(low=0, high=5, shape=shape)
+    x = xp.random.random_uniform(low=0, high=1, shape=shape)
+    n = xp.arange(n_end)[
+        (None,) * len(shape)
+        + (
+            slice(
+                None,
+            ),
+        )
+    ]
+    expected = eval_gegenbauer(n, alpha[..., None], x[..., None])
+    actual = gegenbauer(x, alpha=alpha, n_end=n_end)
+    assert xp.all(xpx.isclose(expected, actual, rtol=1e-3, atol=1e-3))
+
+
+@pytest.mark.parametrize(
+    "shape",
+    [
+        (1,),
+        (2, 3),
+        (3, 3, 4),
+    ],
+)
+@pytest.mark.parametrize("n_end", [8])
+@pytest.mark.parametrize("type", ["gegenbauer", "jacobi"])
+def test_legendre(
+    shape: tuple[int, ...],
+    n_end: int,
+    type: Literal["gegenbauer", "jacobi"],
+    xp: ArrayNamespaceFull,
+) -> None:
+    d = xp.random.randint(3, 8, shape=shape)
+    alpha = (d - 3) / 2
+    x = xp.random.random_uniform(low=0, high=1, shape=shape)
+    n = xp.arange(n_end)[
+        (None,) * len(shape)
+        + (
+            slice(
+                None,
+            ),
+        )
+    ]
+    if type == "jacobi":
+        expected = jacobi(x, alpha=alpha, beta=alpha, n_end=n_end) / binom(n + alpha[..., None], n)
+    elif type == "gegenbauer":
+        expected = gegenbauer(x, alpha=alpha + 1 / 2, n_end=n_end) / binom(
+            n + 2 * alpha[..., None],
+            n,
+            # n + d[..., None] - 3, d[..., None] - 3
+        )
+    else:
+        raise ValueError(f"Invalid type {type}")
+    actual = legendre(x, ndim=xp.asarray(d), n_end=n_end)
+    assert xp.all(xpx.isclose(expected, actual, rtol=1e-3, atol=1e-3))
