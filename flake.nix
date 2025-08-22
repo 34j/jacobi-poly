d2b6ed56d1a69c07fef086781c2036804da2b2cd
34j <34j.github@proton.me>
2025-08-22 14:22:52 +0900

    chore: wip

--- flake.nix
+++ flake.nix
@@ -1,21 +1,5 @@
-# This flake provides a skeleton dev environment for PyTorch with CUDA support and for CUDA deevlopment /
-# compilation with NVCC.
-#
-# To test python:
-# $ nix develop
-# $ python
-# >>> import torch
-# >>> torch.cuda.is_available()
-# >>> torch.cuda.device_count()
-# >>> torch.cuda.get_device_name(0)
-#
-# To test CUDA (hello-world.cu):
-# $ nix develop
-# $ nvcc hello-world.cu -o hello
-# $ ./hello
-
 {
-  description = "A flake providing a dev shell for PyTorch with CUDA and CUDA development using NVCC.";
+  description = "A flake providing a dev shell for Numba with CUDA without installing Numba via nix. Also supports PyTorch yet being minimal for Numba with CUDA.";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -29,49 +13,29 @@
         system = system;
         config.allowUnfree = true;
       };
+      cudatookit-with-cudart-to-lib64 = pkgs.symlinkJoin {
+        name = "cudatoolkit";
+        paths = with pkgs.cudaPackages; [
+          cudatoolkit
+          (pkgs.lib.getStatic cuda_cudart)
+        ];
+        postBuild = ''
+          ln -s $out/lib $out/lib64
+        '';
+      };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          python3
-          (python3.withPackages (
-            ps: with ps; [
-              torchWithCuda
-              # Add other python packages here
-            ]
-          ))
-          cudatoolkit
-          cudaPackages.cudnn
-          cudaPackages.cuda_cudart
-
-          # Need to explicitly override the system gcc (gcc14 in this case) as CUDA requires a
-          # lower version for compatibility
-          gcc13
-        ];
-
         shellHook = ''
-          export CUDA_PATH=${pkgs.cudatoolkit}
+          # Required for both PyTorch and Numba to find CUDA
+          export CUDA_PATH=${cudatookit-with-cudart-to-lib64}
 
-          # Set CC to GCC 13 to avoid the version mismatch error
-          export CC=${pkgs.gcc13}/bin/gcc
-          export CXX=${pkgs.gcc13}/bin/g++
-          export PATH=${pkgs.gcc13}/bin:$PATH
-
-          # Add necessary paths for dynamic linking
+          # Required for both PyTorch and Numba, adds necessary paths for dynamic linking
           export LD_LIBRARY_PATH=${
             pkgs.lib.makeLibraryPath [
-              "/run/opengl-driver" # Needed to find libGL.so
-              pkgs.cudatoolkit
-              pkgs.cudaPackages.cudnn
+              "/run/opengl-driver" # Needed to find libGL.so, required by both PyTorch and Numba
             ]
           }:$LD_LIBRARY_PATH
-
-          # Set LIBRARY_PATH to help the linker find the CUDA static libraries
-          export LIBRARY_PATH=${
-            pkgs.lib.makeLibraryPath [
-              pkgs.cudatoolkit
-            ]
-          }:$LIBRARY_PATH
         '';
       };
     };
